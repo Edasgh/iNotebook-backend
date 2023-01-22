5f6afbf73f77072325ef09db4c91db4a9bbfc571
Eshita <edas25564@gmail.com>
2023-01-22 11:52:12 +0530

    ""

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoUrI="mongodb://localhost:27017";
 
 const connectToMongo=()=>{
   mongoose.connect(mongoUrI,()=>{
-    console.log('Connected to Mongo Successfully');
+    console.log('Connected to Mongo ');
   })
 }
 
