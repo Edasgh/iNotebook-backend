0e15c42aa95f1ff3bd0e71e38c4eedaa9dbae8c9
Eshita <edas25564@gmail.com>
2023-01-30 21:18:17 +0530

    "iNotebook done for now"

--- routes/auth.js
+++ routes/auth.js
@@ -17,17 +17,17 @@ router.post('/createuser',[
     body('email','Enter a valid email').isEmail(),
     body('password','password length should be atleast 8 characters').isLength({ min: 8 })
 ],async (req, res)=>{
-
+    let success=false;
     //if there are errors, return Bad Request and the error messages
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array() });
     }
     try{   
         // Check whether the user with the same email exists already
         let user= await User.findOne({email:req.body.email})
         if(user){
-            return res.status(400).json({error:"A user with this email already exists "});
+            return res.status(400).json({success , error:"A user with this email already exists "});
         }
         //bcrypt adds salt and hash to passwords 
         //For extra security
@@ -50,8 +50,8 @@ router.post('/createuser',[
        const authToken= jwt.sign(data, JWT_SECRET);
        //.sign({paylod object}=>[payload object = any unique data],secretOrPrivateKey=>"String",|[not added here]{options=>expiresIn="3 hours"})[syntax]||GENERATES AUTHENTICATION TOKEN FOR USER TO 'LOG IN' FOR THE NEXT TIME
        //expiresIn="3 hours" makes the user 'log out' from the site after 3 hours{token expires after 3 hours}[not included]
-
-        res.json({authToken});
+        success=true;
+        res.json({success , authToken});
         //no need to write ({authToken:authToken})
 
     } catch(error){
@@ -68,6 +68,7 @@ router.post('/createuser',[
         body('password','Password cannot be blank').exists()
         
     ],async (req, res)=>{
+        let success=false;
       //if there are errors, return Bad Request and the error messages
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -78,13 +79,15 @@ router.post('/createuser',[
     try {
      let user= await User.findOne({email});
      if(!user){
+        success=false;
      return res.status(400).json({error:"Please try to connect with correct credentials"});
 
      }
 
      const passwordCompare= await bcrypt.compare(password,user.password);//Compares the entered password with the saved password(in the db)
      if(!passwordCompare){
-        return res.status(400).json({error:"Please try to connect with correct credentials"});
+        success=false;
+        return res.status(400).json({success , error:"Please try to connect with correct credentials"});
   
      }
 
@@ -96,8 +99,9 @@ router.post('/createuser',[
 //    const jwt_secret = process.env.JWT_SECRET;
 const JWT_SECRET = "Iknowyourdarkestsecretsobeverycarefulofme";
    const authToken= jwt.sign(data, JWT_SECRET);
+    success=true;
   
-    res.json({authToken});
+    res.json({success,authToken});
 
     } catch(error){
         console.error(error.message);
