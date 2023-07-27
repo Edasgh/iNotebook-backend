c934d671d07dccb32f751a45330b906497d1bae5
Edasgh <120154741+Edasgh@users.noreply.github.com>
2023-07-27 16:55:16 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,17 +2,8 @@ const mongoose= require('mongoose');
 require('dotenv').config();
 const mongoUrI=process.env.MONGO_URI;
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-
-//const uri = "mongodb+srv://user_23:47smwFOpExFD4XmA@clusterinb1.sbe2ses.mongodb.net/?retryWrites=true&w=majority";
 
 
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 
 const connectToMongo=()=>{
