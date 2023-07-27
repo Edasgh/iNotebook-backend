1ec9475161045956fd60025f674d7d3cf6cf71bc
Edasgh <120154741+Edasgh@users.noreply.github.com>
2023-07-27 12:28:13 +0530

    Update fetchuser.js

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,5 +1,5 @@
 var jwt = require('jsonwebtoken');
-
+require('dotenv).config()
 const fetchuser =(req,res,next)=>{
    //Get the user from the  jwt token and add id to req object
    const token= req.header('auth-token');
@@ -7,7 +7,7 @@ const fetchuser =(req,res,next)=>{
       res.status(401).send({error:"Please authenticate using a valid token"})
    }
    try {
-    const JWT_SECRET = "Iknowyourdarkestsecretsobeverycarefulofme";
+    const JWT_SECRET = process.env.SECRET;
    //  const jwt_secret=process.env.JWT_SECRET;
     const data= jwt.verify(token, JWT_SECRET);
     //.verify(token=>['authToken' saved in the DataBase],secretOrPrivateKey)
